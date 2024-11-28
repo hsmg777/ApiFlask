15f91cf890db143b9fd4723dc37af510308dd752
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-11-28 10:26:51 -0500

    try 4 render

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Instalar las dependencias necesarias
-apt-get update && apt-get install -y gnupg2 curl software-properties-common
+# Actualizar e instalar dependencias necesarias
+apt-get update && apt-get install -y gnupg2 curl software-properties-common build-essential
 
-# Agregar las claves e instalar el repositorio de Microsoft para los drivers
+# Agregar claves e instalar repositorio de Microsoft para los drivers
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
@@ -11,8 +11,12 @@ apt-get update
 # Aceptar el EULA e instalar el driver ODBC para SQL Server
 ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
 
-# Limpiar para reducir el tamaño del contenedor
+# Verificar instalación del driver y registrar en logs
+odbcinst -j
+ls /opt/microsoft/msodbcsql17
+
+# Limpiar cache para reducir el tamaño del contenedor
 apt-get clean
 
-# Iniciar Gunicorn
+# Iniciar la aplicación con Gunicorn
 gunicorn app:app --bind 0.0.0.0:$PORT
