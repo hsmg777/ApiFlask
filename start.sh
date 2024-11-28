92cba42e70afdbba053580ff206f620e6bc6f75a
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-11-28 10:06:17 -0500

    desplego, pero solucion de problemas con pyodbbc

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-gunicorn app:app -b 0.0.0.0:5000
+apt-get update && \
+apt-get install -y gnupg2 curl && \
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
+apt-get update && \
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev && \
+apt-get clean
+
+gunicorn app:app --bind 0.0.0.0:$PORT
