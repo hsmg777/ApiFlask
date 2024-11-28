a80bf062061b1074aae4a7f5f6118b3ff14f31d4
hsmg777 <138528665+hayland10@users.noreply.github.com>
2024-11-28 09:35:43 -0500

    regreso a Api local

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask_smorest import Api
-from flask_cors import CORS  # Importar Flask-CORS
+from flask_cors import CORS
+from db import init_db
 from controllers.Usuario import blp as UsuarioBlue
 from controllers.Plato import blp as PlatoBlue
 from controllers.Orden import blp as OrdenBlue
 from controllers.Mesa import blp as MesaBlue
 from controllers.RegistroTiempo import blp as TiempoBlue
-from db import init_db, db
 import urllib.parse
+
 
 def createApp():
     app = Flask(__name__)
-    
+
     # Configuración general
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "CORE_REST_API"
@@ -20,41 +21,38 @@ def createApp():
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    
+
     # Configuración de la base de datos
     server = 'tcp:ingwebserver.database.windows.net,1433'
     database = 'IngWeb'
     username = 'aurora'
     password = 'Mamifer_1'  # Reemplaza con la contraseña real
     driver = 'ODBC Driver 17 for SQL Server'
-    
+
     params = urllib.parse.quote_plus(
         f"DRIVER={{{driver}}};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
     )
     connection_string = f"mssql+pyodbc:///?odbc_connect={params}"
     app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-    # Inicializa la base de datos con la aplicación
+
+    # Inicialización de la base de datos
     init_db(app)
-    
-    # Configurar CORS
+
+    # Configuración de CORS
     CORS(app)
-    
-    api = Api(app)
-    
+
     # Registro de blueprints
+    api = Api(app)
     api.register_blueprint(OrdenBlue)
     api.register_blueprint(TiempoBlue)
     api.register_blueprint(PlatoBlue)
     api.register_blueprint(UsuarioBlue)
     api.register_blueprint(MesaBlue)
-    
+
     return app
 
-# Crear instancia de la aplicación
-app = createApp()
 
-# Handler para Vercel
-def handler(environ, start_response):
-    return app(environ, start_response)
+if __name__ == '__main__':
+    app = createApp()
+    app.run(host='0.0.0.0', port=5000, debug=True)
